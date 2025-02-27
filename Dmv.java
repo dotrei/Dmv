655edce5420010051d951b92170319159ac60a8c
reZdotjson <0083rei@proton.me>
2025-02-27 16:17:09 -0500

    actual code

--- Dmv.java
+++ Dmv.java
@@ -1,6 +1,26 @@
 public class Dmv {
     public static void main(String[] args) {
 
+        int randomint = (int) (Math.random() * 100);
+        int plusOne = randomint + 1;
+        int integerCopy = randomint;
+        
+        System.out.println("Hello to the DMV!");
+        System.out.println("Your number is " + randomint);
+
+        while (plusOne <= 100) {
+            System.out.print("Number " + plusOne + "!, ");
+            plusOne++;
+        }
+        while (integerCopy < randomint) {
+            System.out.print("Number " + integerCopy + "!, ");
+            integerCopy++;
+        }
+        System.out.println(" ");
+        System.out.println(" ");
+        System.out.println("Please walk up number " + randomint);
+        System.out.println("Sorry, but you don't have the proper documentation");
+
        
     }
 }
